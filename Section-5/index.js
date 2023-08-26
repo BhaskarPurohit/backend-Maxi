ec07d0fd4add21b3cc558ac6248cec989c64f114
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-08-26 15:16:41 +0530

    middleware

--- Section-5/index.js
+++ Section-5/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
-const app = express()
+const app = express()  //creating express application 
 const port = 3000
+
+const callBackFn = (req, res ,next) =>{
+    // res.send("request sent successfully")
+    console.log("inside the middleware")
+    next()
+}
+
+const secondCallback = (req,res,next)=>{
+    console.log("second middleware")
+    next()  //next()allows the request to continue to the next niddleware in the line
+}
+
+
+app.use(callBackFn)  //---> allows us to create middleware
+app.use(secondCallback)
 
 const handleRequest = (req, res)=>{
     res.send("express chal gya")
