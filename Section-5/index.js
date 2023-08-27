216e6331049d46b90c65ba909ffd363ecec9997a
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-08-27 16:27:43 +0530

    new

--- Section-5/index.js
+++ Section-5/index.js
@@ -1,26 +1,54 @@
 const express = require('express')
 const app = express()  //creating express application 
 const port = 3000
+const bodyParser = require('body-parser')
 
-const callBackFn = (req, res ,next) =>{
-    // res.send("request sent successfully")
-    console.log("inside the middleware")
-    next()
-}
+app.use(bodyParser.urlencoded({extended: false}))
 
-const secondCallback = (req,res,next)=>{
-    console.log("second middleware")
-    // next()  //next()allows the request to continue to the next niddleware in the line
-    res.send('<h1>home page</h1>')
-}
+// const callBackFn = (req, res ,next) =>{
+//     // res.send("request sent successfully")
+//     console.log("inside the middleware")
+//     next()
+// }
+
+// const secondCallback = (req,res,next)=>{
+//     console.log("second middleware")
+//     // next()  //next()allows the request to continue to the next niddleware in the line
+//     // res.send('<h1>home page</h1>')
+//     res.redirect('/')
+// }
 
 
-app.use(callBackFn)  //---> allows us to create middleware
-app.use('/home',secondCallback)
+// const addProduct = (req, res, next)=>{
+//     res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Submit</button></form>')
+// }
+
+
+// app.use(callBackFn)  //---> allows us to create middleware
+// app.use('/product',secondCallback)
+// app.use('/add-product', addProduct)
 
 const handleRequest = (req, res)=>{
     res.send("express chal gya")
 }
+
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit">Submit</button></form>')
+})
+
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+
+})
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>hello from express</h1>')
+})
+
+
+
 
 app.get('/', handleRequest)
 
